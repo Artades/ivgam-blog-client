3c5db69550926fd58d41f50961d0fa214a53ad59
Artades <artemijgalaj@gmail.com>
2024-03-23 23:21:01 +0600

    feat: Auth hook has been added

---
+++ helpers/checkAuth.ts
@@ -0,0 +1,39 @@
+import { getAccessToken } from "./cookies";
+
+
+export const checkAuth = async () => {
+  const authToken: string | undefined  = await getAccessToken()
+    .then((result) => {
+      console.log('Access Token :', result.accessToken);
+      return result.accessToken;
+    })
+    .catch((error) => {
+      console.error(error);
+      return undefined;
+    });
+
+  try {
+    // Check if the user is authenticated based on a cookie key
+    if (authToken) {
+      return {
+        props: {},
+      };
+    } else {
+      return {
+        redirect: {
+          destination: '/auth',
+          permanent: false,
+        },
+      };
+    }
+  } catch (err) {
+    console.error('Authentication error:', err);
+
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+};
---
+++ helpers/cookies.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+
+
+
+export const getAccessToken = (): Promise<{
+  accessToken: string | undefined;
+}> => {
+  const cookieStore = cookies();
+  const _token: any = cookieStore.get('accessToken');
+  if (_token) {
+    return Promise.resolve({ accessToken: _token.value });
+  } else {
+    return Promise.resolve({ accessToken: undefined });
+  }
+};
+
+
+export const setAccessToken = (token: string) => {
+  cookies().set('accessToken', token, {
+    secure: true,
+    path: '/',
+  });
+};
+
+
+export const burnAccessToken = () => {
+  cookies().delete('accessToken');
+};
