76cac3ca4f42d80c2f1a3a758465cea2e171a436
Artades <artemijgalaj@gmail.com>
2024-05-10 23:55:18 +0500

    feat: MobileSheet has been redesigned and added to Helmet component

--- components/Helmet/Helmet.tsx
+++ components/Helmet/Helmet.tsx
@@ -3,6 +3,7 @@
 import React, { FC } from 'react';
 import { BreadCrumbs, BreadCrumbsProps } from '../BreadCrumbs/BreadCrumbs';
 import { usePathname, useRouter } from 'next/navigation';
+import { MobileSheet } from '../Layout/MobileSheet/MobileSheet';
 
 interface HelmetProps {
   pageTitle: string;
@@ -14,10 +15,13 @@ const Helmet: FC<HelmetProps> = ({ pageTitle, breadCrumbs }) => {
   return (
     <nav className="w-full bg-zinc-950 border-b border-b-zinc-800 ">
       <div className="w-full px-5 py-7 flex items-start flex-col gap-y-6">
-        <div className="w-full h-full">
+        <div className="w-full h-full flex items-center justify-between">
           <h2 className="sm:text-3xl  text-2xl text-pretty font-bold text-white">
             {pageTitle}
           </h2>
+          <nav className='sm:hidden block'>
+            <MobileSheet /> 
+          </nav>
         </div>
 
         {breadCrumbs && <BreadCrumbs items={breadCrumbs?.items} />}
--- components/Layout/MobileSheet/MobileSheet.tsx
+++ components/Layout/MobileSheet/MobileSheet.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button } from '@/components/ui/button';
 
 import {
@@ -15,14 +16,22 @@ import SidebarItem from '../Sidebar/SidebarItem';
 import SidebarLogo from '../Sidebar/SidebarLogo';
 import { items } from '@/config/nav';
 import { HiOutlineMenuAlt2 } from 'react-icons/hi';
+import LogoutButton from '../Sidebar/LogoutButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
+import { RootState } from '@/store';
+import { openLoginModal, openRegisterModal } from '@/store/slices/authModalsSlice';
 
 export function MobileSheet() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const { authStatus } = useSelector((state: RootState) => state.authStatus);
+
   return (
     <Sheet>
-      <SheetTrigger asChild className='p-2 bg-zinc-900 rounded-lg'>
-       
-          <HiOutlineMenuAlt2 className='w-10 h-10 text-white' />
-       
+      <SheetTrigger asChild className="p-2 bg-zinc-900 rounded-lg">
+        <HiOutlineMenuAlt2 className="w-10 h-10 text-white" />
       </SheetTrigger>
       <SheetContent side={'left'}>
         <div className="col-span-1 h-full w-full ">
@@ -46,14 +55,36 @@ export function MobileSheet() {
               label=""
             /> */}
 
-              <div className="w-full flex flex-col space-y-2 py-10">
-                <Button size={'lg'} variant={'default'}>
-                  Войти
-                </Button>
-                <Button size={'lg'} variant={'outline'}>
-                  Создать аккаунт
-                </Button>
-              </div>
+              {authStatus === 'not authenticated' ? (
+                <div className="w-full flex flex-col space-y-2 py-10">
+                  <Button
+                    size={'lg'}
+                    variant={'default'}
+                    onClick={() => dispatch(openLoginModal())}
+                  >
+                    Войти
+                  </Button>
+                  <Button
+                    size={'lg'}
+                    variant={'outline'}
+                    onClick={() => dispatch(openRegisterModal())}
+                  >
+                    Создать аккаунт
+                  </Button>
+                </div>
+              ) : (
+                <div className="w-full flex flex-col space-y-2 py-10">
+                  <Button
+                    size={'lg'}
+                    variant={'default'}
+                    onClick={() => router.push('/suggest')}
+                  >
+                    Предложить пост
+                  </Button>
+
+                  <LogoutButton />
+                </div>
+              )}
             </div>
           </div>
         </div>
