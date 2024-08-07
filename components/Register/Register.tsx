a5f81d6df96b4f6653b7743655be558671a7bee9
Artades <artemijgalaj@gmail.com>
2024-08-07 14:22:01 +0500

    fix:Registration form has been fixed

--- components/Register/Register.tsx
+++ components/Register/Register.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import * as Api from '@/api';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -7,7 +6,7 @@ import { ClipLoader } from 'react-spinners';
 import * as z from 'zod';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link'; // Import Link from next/link
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
@@ -18,7 +17,6 @@ import {
   FormMessage,
 } from '../ui/form';
 import { showErrorToast } from '../Error/showErrorToast';
-import useAuthentication from '@/hooks/useAuth';
 
 const registerFormSchema = z.object({
   first_name: z.string().min(1, {
@@ -42,7 +40,6 @@ const registerFormSchema = z.object({
 });
 
 export function Register() {
-  
   const [isLoading, setLoading] = useState<boolean>(false);
   const router = useRouter();
   const registerForm = useForm<z.infer<typeof registerFormSchema>>({
@@ -89,94 +86,92 @@ export function Register() {
   return (
     <div className="w-full h-[100dvh] flex items-center justify-center">
       <Form {...registerForm}>
-        <form
-          onSubmit={(e: FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            registerForm.handleSubmit(handleRegister);
-          }}
-          className="w-full space-y-4 max-w-md gap-4 border bg-zinc-950/70 p-6 rounded-lg sm:mx-0 mx-5"
-        >
-          <h2 className="text-2xl text-pretty font-bold text-white">
-            Создать аккаунт
-          </h2>
-          <div className="flex space-x-2 w-full">
-            <FormField
-              control={registerForm.control}
-              name="first_name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Имя" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={registerForm.control}
-              name="last_name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Фамилия" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <FormField
-            control={registerForm.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={registerForm.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type="password" placeholder="Пароль" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="flex items-center flex-col gap-5">
+          <form
+            onSubmit={registerForm.handleSubmit(handleRegister)}
+            className="w-full space-y-4 max-w-md gap-4 border bg-zinc-950/70 p-6 rounded-lg"
+          >
+            <h2 className="text-2xl font-semibold">Создать аккаунт</h2>
 
-          <div className="flex flex-col items-start gap-y-4">
-            <Button
-              disabled={isLoading}
-              type="submit"
-              className={
-                isLoading
-                  ? 'bg-transparent border text-white border-white flex items-center gap-x-5'
-                  : 'default'
-              }
-            >
-              {isLoading ? (
-                <div className="flex items-center gap-x-3">
-                  Загрузка
-                  <ClipLoader size={20} color="#fff" />
-                </div>
-              ) : (
-                'Создать'
-              )}
-            </Button>
-            <div className="text-sm text-gray-400">
-              Уже есть аккаунт?{' '}
-              <Link href="/auth/login" className="text-zinc-600 underline">
-                Войти
-              </Link>
+            <div className="flex space-x-2 w-full">
+              <FormField
+                control={registerForm.control}
+                name="first_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Имя" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={registerForm.control}
+                name="last_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Фамилия" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          </div>
-        </form>
+            <FormField
+              control={registerForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={registerForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Пароль" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col items-start gap-y-4">
+              <Button
+                disabled={isLoading}
+                type="submit"
+                className={
+                  isLoading
+                    ? 'bg-transparent border text-white border-white flex items-center gap-x-5'
+                    : 'default'
+                }
+              >
+                {isLoading ? (
+                  <div className="flex items-center gap-x-3">
+                    Загрузка
+                    <ClipLoader size={20} color="#fff" />
+                  </div>
+                ) : (
+                  'Создать'
+                )}
+              </Button>
+              <div className="text-sm text-gray-400">
+                Уже есть аккаунт?{' '}
+                <Link href="/login" className="text-zinc-700 hover:underline">
+                  Войти
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
       </Form>
     </div>
   );
